285a1354091fdd1dfcf7115599ac34e88303c480
Michael Gibson <m.gibson323@gmail.com>
2019-03-08 21:21:35 -0800

    Initial commit

---
+++ login_info.sh
@@ -0,0 +1,10 @@
+#! /bin/bash	
+figlet $(hostname)
+echo "---------------"
+whoami
+echo "---------------"
+#255.255.255.255
+#ifconfig en0| grep 'inet '|cut -d' ' -f2 |awk '{print $1}'
+#ifconfig eth0| grep 'inet '|awk '{print $2}'
+ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
+echo "---------------"
